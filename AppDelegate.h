c61b85b7073b83189312754ad3dee54cacfcc199
sgys <975697951@qq.com>
2015-05-28 20:19:51 +0800

    人人人人

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPro
+//
+//  Created by sgys on 15/5/24.
+//  Copyright (c) 2015年 sgys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
